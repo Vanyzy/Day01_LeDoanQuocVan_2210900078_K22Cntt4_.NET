64b87f6053e23fb0dfa8129277f972850d614a54
Vanyzy <vanyus1505@gmail.com>
2025-09-17 16:06:07 +0700

    Day01

---
+++ Day01Demo1/Day01Demo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Khi vào trang chủ ("/") sẽ hiện dòng chữ
+app.MapGet("/", () => "Hello World - Lê Đoàn Quốc Văn - 2210900078");
+
+app.Run();
+
